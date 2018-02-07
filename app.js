943390548fa0723c6d9ae53c86884a538f844c33
hacker-guy <bugeeja@gmail.com>
2018-02-07 18:56:32 +1100

    v2

--- app.js
+++ app.js
@@ -1,3 +1,7 @@
+var nextpage = null;
+var prevpage = null;
+var searchtext = null;
+
 function videoSearch() {
 	$("#searchform").submit(function(event) {
 		event.preventDefault();
@@ -17,16 +21,102 @@ function searchYoutube (text) {
 					$(".results").html("Nothing to see here :(");
 				}
 				for (var i = 0; i < json.items.length; i++) {
-					$(".results").append("<div class=\"thumbn\"> <a href=\"https://www.youtube.com/watch?v=" + json.items[i].id.videoId + 
-						"\"><img src=\"" + json.items[i].snippet.thumbnails.medium.url + "\"></div>" );
+					$(".results").append("<div class=\"thumbn\"> <a href=\"https://www.youtube.com/watch?v=" + 
+						json.items[i].id.videoId + "\"><img src=\"" + json.items[i].snippet.thumbnails.medium.url + "\"> <br> <span>" +
+					 	json.items[i].snippet.title + "</span></div>" );
+					$("#nav_form").removeAttr("hidden");
 				}
-				
+				if (json.nextPageToken) {
+					$("#next").removeAttr("disabled");
+					nextpage = json.nextPageToken;
+				} else {
+					$("#next").prop('disabled', true);
+				}
+				if (json.prevPageToken) {
+					$("#prev").removeAttr("disabled");
+					prevpage = json.prevPageToken;
+				} else {
+					$("#prev").prop('disabled', true);
+				}
 			} else {
 				$(".results").html("Oops something went wrong here :O");
 			}
 		});
 }
 
+$(document).ready(function()  {
+	$(document).on("click", "#prev", function(event) {
+		event.preventDefault();
+		$.getJSON("https://www.googleapis.com/youtube/v3/&search?pageToken=" + prevpage + "&part=snippet\
+		&q="+searchtext+"maxResults=10&type=video&key=AIzaSyA_umswAZe_kez-yvwjY8nZj0Qr6lKaJnc"
+		, function(json) {
+			console.log(json);
+			if (json != "Nothing found.") {
+				$(".results").html("");
+				if (json.items.length == 0) {
+					$(".results").html("Nothing to see here :(");
+				}
+				for (var i = 0; i < json.items.length; i++) {
+					$(".results").append("<div class=\"thumbn\"> <a href=\"https://www.youtube.com/watch?v=" + 
+						json.items[i].id.videoId + "\"><img src=\"" + json.items[i].snippet.thumbnails.medium.url + "\"> <br> <span>" +
+					 	json.items[i].snippet.title + "</span></div>" );
+					$("#nav_form").removeAttr("hidden");
+				}
+				if (json.prevPageToken) {
+					$("#prev").removeAttr("disabled");
+					prevpage = json.prevPageToken;
+				} else {
+					$("#prev").prop('disabled', true);
+				}
+			} else {
+				$(".results").html("Oops something went wrong here :O");
+			}
+		});
+	});
+	$(document).on("click", "#next", function(event) {
+		event.preventDefault();
+		$.getJSON("https://www.googleapis.com/youtube/v3/&search?pageToken=" + nextpage + "&part=snippet\
+		&q="+searchtext+"&maxResults=10&type=video&key=AIzaSyA_umswAZe_kez-yvwjY8nZj0Qr6lKaJnc"
+		, function(json) {
+			console.log(json);
+			if (json != "Nothing found.") {
+				$(".results").html("");
+				if (json.items.length == 0) {
+					$(".results").html("Nothing to see here :(");
+				}
+				for (var i = 0; i < json.items.length; i++) {
+					$(".results").append("<div class=\"thumbn\"> <a href=\"https://www.youtube.com/watch?v=" + 
+						json.items[i].id.videoId + "\"><img src=\"" + json.items[i].snippet.thumbnails.medium.url + "\"> <br> <span>" +
+					 	json.items[i].snippet.title + "</span></div>" );
+					$("#nav_form").removeAttr("hidden");
+				}
+				if (json.nextPageToken) {
+					$("#next").removeAttr("disabled");
+					nextpage = json.nextPageToken;
+				} else {
+					$("#next").prop('disabled', true);
+				}
+			} else {
+				$(".results").html("Oops something went wrong here :O");
+			}
+		});
+	});
+});
+
+function windowScroll() {
+	$(window).scroll(function() {
+	   if($(window).scrollTop() + $(window).height() == $(document).height()) {
+	       $(".footer").fadeIn("slow").removeAttr("hidden");
+	     
+	    }else{
+	        
+	        $(".footer").fadeOut("slow").prop('hidden', true);
+	        
+	   }
+	});
+}
+
 $(function() {
 	videoSearch();
+	windowScroll();
 });
